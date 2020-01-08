08d5ff6625f7a5a52dd2de5afdf3a83c312f9cb2
rlecheta <rlecheta@livetouch.com.br>
2020-01-08 17:46:26 -0300

    CRUD De Compras

--- src/main/java/com/udemy/compras/graphql/CompraResolver.java
+++ src/main/java/com/udemy/compras/graphql/CompraResolver.java
@@ -1,13 +1,28 @@
 package com.udemy.compras.graphql;
 
 import com.coxautodev.graphql.tools.GraphQLResolver;
-import com.udemy.compras.domain.Compra;
+import com.udemy.compras.domain.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CompraResolver implements GraphQLResolver<Compra> {
 
+    @Autowired
+    private ClienteService clienteService;
+
+    @Autowired
+    private ProdutoService produtoService;
+
     public String status(Compra c) {
         return "Teste : " + c.getStatus();
+    }
+
+    public Cliente cliente(Compra c) {
+        return clienteService.findById(c.getCliente().getId());
+    }
+
+    public Produto produto(Compra c) {
+        return produtoService.findById(c.getProduto().getId());
     }
 }
