dfefcd69ca17f0de163531fc49a962f7877e36c7
rlecheta <rlecheta@livetouch.com.br>
2020-01-09 17:05:27 -0300

    Tratamento de erros com GraphQL

---
+++ src/main/java/com/udemy/compras/graphql/exceptions/DomainException.java
@@ -0,0 +1,9 @@
+package com.udemy.compras.graphql.exceptions;
+
+public class DomainException extends RuntimeException {
+
+    public DomainException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/main/java/com/udemy/compras/graphql/exceptions/GraphQLHandler.java
@@ -0,0 +1,56 @@
+package com.udemy.compras.graphql.exceptions;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import graphql.ExceptionWhileDataFetching;
+import graphql.GraphQLError;
+import graphql.servlet.GenericGraphQLError;
+import graphql.servlet.GraphQLErrorHandler;
+import graphql.validation.ValidationError;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Component
+public class GraphQLHandler implements GraphQLErrorHandler {
+
+    @Override
+    public List<GraphQLError> processErrors(List<GraphQLError> list) {
+        return list.stream().map(this::getNested).collect(Collectors.toList());
+    }
+
+    private GraphQLError getNested(GraphQLError error) {
+        if (error instanceof ExceptionWhileDataFetching) {
+            ExceptionWhileDataFetching exceptionError = (ExceptionWhileDataFetching) error;
+            if (exceptionError.getException() instanceof DomainException) {
+                Throwable ex = exceptionError.getException();
+                String msg = ex.getMessage();
+                return new SimpleError(msg);
+            }
+        } else if (error instanceof ValidationError) {
+            String msg = error.getMessage();
+            return new SimpleError(msg);
+        }
+        return error;
+    }
+}
+
+class SimpleError extends GenericGraphQLError {
+
+    SimpleError(String message) {
+        super(message);
+    }
+
+    @Override
+    @JsonIgnore
+    public List<Object> getPath() {
+        return null;
+    }
+
+    @Override
+    @JsonIgnore
+    public Map<String, Object> getExtensions() {
+        return null;
+    }
+}
