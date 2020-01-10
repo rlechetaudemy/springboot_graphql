6a259d9939e94e05ec18f5d1222562b760b80c86
rlecheta <rlecheta@livetouch.com.br>
2020-01-10 19:39:27 -0300

    Heroku

--- src/main/java/com/udemy/compras/HelloController.java
+++ src/main/java/com/udemy/compras/HelloController.java
@@ -5,10 +5,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
 public class HelloController {
 
     @GetMapping
+    @RequestMapping("/")
+    public String index() {
+        return "Acesse /graphiql ou /playground para testar a API.";
+    }
+
+    @GetMapping
+    @RequestMapping("/hello")
     public String hello() {
         return "Hello REST";
     }
